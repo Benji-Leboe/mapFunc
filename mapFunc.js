ae58d7f2e249184dff32eef3eed0d78d45f8b653
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-08-30 21:20:46 +0000

    wrote own map function

---
+++ mapFunc.js
@@ -0,0 +1,22 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+console.log(myMap(words, function(word) {
+  return word.length;
+}));
+
+console.log(myMap(words, function(word) {
+  return word.toUpperCase();
+}));
+
+console.log(myMap(words, function(word) {
+  return word.split('').reverse().join('');
+}));
+
+function myMap(arr, cb){
+  newArr = [];
+  arr.forEach(function(item){
+    newArr.push(cb(item));
+  })
+  return newArr;
+}
+
